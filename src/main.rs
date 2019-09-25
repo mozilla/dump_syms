358eb970cacaaf2b3bf4c6a956a12cbd12d1450a
Calixte Denizet <calixte.denizet@gmail.com>
2019-09-25 11:38:41 +0200

    Add an argument to choose output file and dump by default on stdout

--- src/main.rs
+++ src/main.rs
@@ -15,15 +15,18 @@ use clap::{App, Arg};
 
 use simplelog::{Config, LevelFilter, TermLogger, TerminalMode};
 use std::fs::File;
-use std::io::BufWriter;
+use std::io::{self, BufWriter, Write};
 use std::path::PathBuf;
 
-fn get_writer_for_sym(file_name: &str) -> BufWriter<File> {
-    let mut path = PathBuf::from(file_name);
-    path.set_extension("sym");
-
-    let output = File::create(&path)
-        .unwrap_or_else(|_| panic!("Cannot open file {} for writing", path.to_str().unwrap()));
+fn get_writer_for_sym(file_name: &str) -> BufWriter<Box<dyn Write>> {
+    let output: Box<dyn Write> = if file_name.is_empty() || file_name == "-" {
+        Box::new(io::stdout())
+    } else {
+        let path = PathBuf::from(file_name);
+        let output = File::create(&path)
+            .unwrap_or_else(|_| panic!("Cannot open file {} for writing", path.to_str().unwrap()));
+        Box::new(output)
+    };
     BufWriter::new(output)
 }
 
@@ -41,8 +44,17 @@ fn main() {
                 .required(true)
                 .takes_value(true),
         )
+        .arg(
+            Arg::with_name("output")
+                .help("Output file or - for stdout")
+                .short("o")
+                .long("output")
+                .default_value("-")
+                .takes_value(true),
+        )
         .get_matches();
 
+    let output = matches.value_of("output").unwrap();
     let filename = matches.value_of("filename").unwrap();
     let path = PathBuf::from(filename);
     let buf = utils::read_file(&path);
@@ -52,14 +64,14 @@ fn main() {
         "dll" | "exe" => {
             let res = windows::utils::get_pe_pdb_buf(path, &buf);
             if let Some((pe, pdb_buf, pdb_name)) = res {
-                let output = get_writer_for_sym(&pdb_name);
+                let output = get_writer_for_sym(&output);
                 windows::pdb::PDBInfo::dump(&pdb_buf, pdb_name, filename, Some(pe), output)
             } else {
                 Err(From::from("No pdb file found"))
             }
         }
         "pdb" => {
-            let output = get_writer_for_sym(&filename);
+            let output = get_writer_for_sym(&output);
             windows::pdb::PDBInfo::dump(&buf, filename, "".to_string(), None, output)
         }
         _ => Ok(()),
