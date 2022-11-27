9b5c3c09a910f7233dda136fca8185ce27c95a0b
Markus Stange <mstange.moz@gmail.com>
2022-11-26 23:50:24 -0500

    clap 4 upgrade: Migration step 3.
    This addresses all deprecation warnings, as printed by the following command:
    `cargo check --features clap/deprecated`

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@
 // copied, modified, or distributed except according to those terms.
 
 use clap::ArgAction;
-use clap::{crate_authors, crate_version, App, Arg};
+use clap::{crate_authors, crate_version, Arg, Command};
 use log::error;
 use simplelog::{ColorChoice, ConfigBuilder, LevelFilter, TermLogger, TerminalMode};
 use std::ops::Deref;
@@ -16,147 +16,144 @@ use action::Action;
 use dump_syms::common;
 use dump_syms::dumper;
 
-fn cli() -> App<'static> {
-    App::new("dump_syms")
+fn cli() -> Command<'static> {
+    Command::new("dump_syms")
     .version(crate_version!())
     .author(crate_authors!("\n"))
     .about("Dump debug symbols to breakpad symbols")
     .arg(
-        Arg::with_name("filenames")
+        Arg::new("filenames")
             .help("Files to dump (.dll, .exe, .pdb, .pd_, .so, .dbg)")
             .required(true)
-            .multiple(true)
+            .multiple_values(true)
             .takes_value(true)
-            .action(ArgAction::StoreValue)
+            .action(ArgAction::Set)
     )
     .arg(
-        Arg::with_name("output")
+        Arg::new("output")
             .help("Output file or - for stdout")
             .short('o')
             .long("output")
             .takes_value(true)
-            .action(ArgAction::StoreValue),
+            .action(ArgAction::Set),
     )
     .arg(
-        Arg::with_name("store")
+        Arg::new("store")
             .help("Store output file as FILENAME.pdb/DEBUG_ID/FILENAME.sym in the given directory")
             .short('s')
             .long("store")
             .takes_value(true)
-            .action(ArgAction::StoreValue),
+            .action(ArgAction::Set),
     )
     .arg(
-        Arg::with_name("debug_id")
+        Arg::new("debug_id")
             .help("Get the pdb file passed as argument from the cache or from symbol server using the debug id")
             .long("debug-id")
             .takes_value(true)
-            .action(ArgAction::StoreValue),
+            .action(ArgAction::Set),
     )
     .arg(
-        Arg::with_name("code_id")
+        Arg::new("code_id")
             .help("Get the dll/exe file passed as argument from the cache or from symbol server using the code id")
             .long("code-id")
             .takes_value(true)
-            .action(ArgAction::StoreValue),
+            .action(ArgAction::Set),
     )
     .arg(
-        Arg::with_name("symbol-server")
+        Arg::new("symbol-server")
             .help("Symbol Server configuration\n(e.g. \"SRV*c:\\symcache\\*https://symbols.mozilla.org/\")\nIt can be in file $HOME/.dump_syms/config too.")
             .long("symbol-server")
             .takes_value(true)
-            .action(ArgAction::StoreValue),
+            .action(ArgAction::Set),
     )
     .arg(
-        Arg::with_name("check_cfi")
+        Arg::new("check_cfi")
             .help("Fail if there are no CFI data")
             .long("check-cfi")
-            .action(ArgAction::StoreValue)
+            .action(ArgAction::SetTrue)
     )
     .arg(
-        Arg::with_name("verbose")
+        Arg::new("verbose")
             .help("Set the level of verbosity (off, error (default), warn, info, debug, trace)")
             .long("verbose")
             .default_value("error")
             .takes_value(true)
-            .action(ArgAction::StoreValue),
+            .action(ArgAction::Set),
     )
     .arg(
-        Arg::with_name("arch")
+        Arg::new("arch")
             .help("Set the architecture to select in fat binaries")
             .short('a')
             .long("arch")
             .default_value(common::get_compile_time_arch())
             .takes_value(true)
-            .action(ArgAction::StoreValue),
+            .action(ArgAction::Set),
     )
     .arg(
-        Arg::with_name("type")
+        Arg::new("type")
             .help("Ignored, listed for compatibility only")
             .short('t')
             .long("type")
             .default_value("")
             .takes_value(true)
-            .action(ArgAction::StoreValue),
+            .action(ArgAction::Set),
     ).arg(
-        Arg::with_name("list_arch")
+        Arg::new("list_arch")
             .help("List the architectures present in the fat binaries")
             .long("list-arch")
-            .action(ArgAction::StoreValue)
+            .action(ArgAction::SetTrue)
     )
     .arg(
-        Arg::with_name("num_jobs")
+        Arg::new("num_jobs")
             .help("Number of jobs")
             .short('j')
             .value_name("NUMBER")
             .default_value("")
             .takes_value(true)
-            .action(ArgAction::StoreValue),
+            .action(ArgAction::Set),
     )
     .arg(
-        Arg::with_name("mapping_var")
+        Arg::new("mapping_var")
             .help("A pair var=value such as rev=123abcd")
             .long("mapping-var")
-            .multiple(true)
             .takes_value(true)
-            .action(ArgAction::StoreValue),
+            .action(ArgAction::Append),
     )
     .arg(
-        Arg::with_name("mapping_src")
+        Arg::new("mapping_src")
             .help("Regex to match a path with capturing groups")
             .long("mapping-src")
-            .multiple(true)
             .takes_value(true)
-            .action(ArgAction::StoreValue),
+            .action(ArgAction::Append),
     )
     .arg(
-        Arg::with_name("mapping_dest")
+        Arg::new("mapping_dest")
             .help(r#"A replacement string using groups, variables (set with --mapping-var), special variable like DIGEST or digest.
 For example with --mapping-var="rev=123abc" --mapping-src="/foo/bar/(.*)" --mapping-dest="https://my.source.org/{rev}/{digest}/{1}" a path like "/foo/bar/myfile.cpp" will be transformed into "https://my.source.org/123abc/sha512_of_myfile.cpp/myfile.cpp"
 "#)
             .long("mapping-dest")
-            .multiple(true)
             .takes_value(true)
-            .action(ArgAction::StoreValue),
+            .action(ArgAction::Append),
     )
     .arg(
-        Arg::with_name("mapping_file")
+        Arg::new("mapping_file")
             .help("A json file containing mapping")
             .long("mapping-file")
             .takes_value(true)
-            .action(ArgAction::StoreValue),
+            .action(ArgAction::Set),
     )
     .arg(
-        Arg::with_name("inlines")
+        Arg::new("inlines")
             .help("Whether to emit INLINE and INLINE_ORIGIN directives")
             .long("inlines")
-            .action(ArgAction::StoreValue),
+            .action(ArgAction::SetTrue),
     )
 }
 
 fn main() {
     let matches = cli().get_matches();
 
-    let verbosity = match matches.value_of("verbose").unwrap() {
+    let verbosity = match matches.get_one::<String>("verbose").unwrap().as_str() {
         "off" => LevelFilter::Off,
         "warn" => LevelFilter::Warn,
         "info" => LevelFilter::Info,
@@ -197,32 +194,34 @@ fn main() {
         error!("A panic occurred at {}:{}: {}", filename, line, cause);
     }));
 
-    let output = matches.value_of("output");
-    let filenames: Vec<_> = matches.values_of("filenames").unwrap().collect();
-    let symbol_server = matches.value_of("symbol-server");
-    let store = matches.value_of("store");
-    let debug_id = matches.value_of("debug_id");
-    let code_id = matches.value_of("code_id");
-    let arch = matches.value_of("arch").unwrap();
-    let check_cfi = matches.is_present("check_cfi");
-    let emit_inlines = matches.is_present("inlines");
-    let mapping_var = matches
-        .values_of("mapping_var")
-        .map(|v| v.collect::<Vec<_>>());
-    let mapping_src = matches
-        .values_of("mapping_src")
-        .map(|v| v.collect::<Vec<_>>());
-    let mapping_dest = matches
-        .values_of("mapping_dest")
-        .map(|v| v.collect::<Vec<_>>());
-    let mapping_file = matches.value_of("mapping_file");
-    let num_jobs = if let Ok(num_jobs) = matches.value_of("num_jobs").unwrap().parse::<usize>() {
+    let output = matches.get_one::<String>("output").map(String::as_str);
+    let filenames = to_vec(matches.get_many::<String>("filenames").unwrap());
+    let symbol_server = matches
+        .get_one::<String>("symbol-server")
+        .map(String::as_str);
+    let store = matches.get_one::<String>("store").map(String::as_str);
+    let debug_id = matches.get_one::<String>("debug_id").map(String::as_str);
+    let code_id = matches.get_one::<String>("code_id").map(String::as_str);
+    let arch = matches.get_one::<String>("arch").unwrap().as_str();
+    let check_cfi = matches.get_flag("check_cfi");
+    let emit_inlines = matches.get_flag("inlines");
+    let mapping_var = matches.get_many("mapping_var").map(to_vec);
+    let mapping_src = matches.get_many("mapping_src").map(to_vec);
+    let mapping_dest = matches.get_many("mapping_dest").map(to_vec);
+    let mapping_file = matches
+        .get_one::<String>("mapping_file")
+        .map(String::as_str);
+    let num_jobs = if let Ok(num_jobs) = matches
+        .get_one::<String>("num_jobs")
+        .unwrap()
+        .parse::<usize>()
+    {
         num_jobs
     } else {
         num_cpus::get()
     };
 
-    let action = if matches.is_present("list_arch") {
+    let action = if matches.get_flag("list_arch") {
         Action::ListArch
     } else {
         let output = match (output, store) {
@@ -255,6 +254,10 @@ fn main() {
         eprintln!("{}", e);
         std::process::exit(1);
     }
+}
+
+fn to_vec(values: clap::parser::ValuesRef<String>) -> Vec<&str> {
+    values.map(String::as_str).collect()
 }
 
 #[test]
